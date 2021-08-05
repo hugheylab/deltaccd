c3da4b78aab92663725c59888a5fe9144895005e
Elliot Outland <ehoutland@gmail.com>
2021-08-05 10:36:33 -0500

    updating tests for nPerm = 0, geneNames != null, fixing bug in calcCCD

--- R/deltaccd.R
+++ R/deltaccd.R
@@ -130,7 +130,7 @@ calcCCD = function(
 
 
   refCor = checkRefCor(refCor, refEmat, geneNames, method)
-  geneNames = checkGenes(emat, refCor)
+  geneNames = checkGenes(emat, refCor, geneNames)
   
 
   if (is.null(groupVec)) {
@@ -142,7 +142,7 @@ calcCCD = function(
   
   if (nPerm > 1) {
     checkVar(emat, groupVec)
-  } else { checkVar(emat[geneNames, ]) }
+  } else { checkVar(emat[geneNames, ], groupVec) }
 
   nComb = choose(nrow(emat), length(geneNames))
 
