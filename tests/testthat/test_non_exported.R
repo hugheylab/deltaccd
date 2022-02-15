5495ef1f11e1adf49e2b7db8edaed4e00b3040e1
eoutland <ehoutland@gmail.com>
2022-02-15 05:10:24 -0500

    updating tests for calcDeltaCCDSimple

--- tests/testthat/test_non_exported.R
+++ tests/testthat/test_non_exported.R
@@ -14,7 +14,7 @@ genCors = function() {
 test_that('calcDist', {
   d = calcDist(c(1, 0), c(1, 0))
   expect_equal(d, 0)
-  
+
   d = calcDist(c(1, 0), c(0, 1))
   expect_equal(d, sqrt(2))
 })
@@ -22,13 +22,13 @@ test_that('calcDist', {
 test_that('calcCCDSimple', {
   ccd = calcCCDSimple(diag(1, 2), diag(1, 2))
   expect_equal(ccd, 1)
-  
+
   ref = cbind(c(1, 1), c(1, 1))
   emat = rbind(c(1, 0, 1), c(0, 1, 0))
   ccd = calcCCDSimple(ref, emat)
   expect_equal(ccd, 2)
-  
-  ref = cbind(c(1, 0, -1), c(0, 1, -1), c(-1, -1, 1)) 
+
+  ref = cbind(c(1, 0, -1), c(0, 1, -1), c(-1, -1, 1))
   emat = rbind(c(1, 0, 0), c(0, 1, -1), c(0, 1, 1))
   ccd = calcCCDSimple(ref, emat, scale = TRUE)
   expect_equal(ccd, 1/3)
@@ -38,29 +38,29 @@ test_that('checkVar', {
   groupVec = c('a', 'a', 'b', 'b')
   emat = cbind(diag(1, 2), diag(1, 2))
   expect_invisible(checkVar(emat, groupVec))
-  
+
   emat = cbind(diag(1, 2), matrix(1, 2, 2))
   rownames(emat) = paste0('gene_', 1:2)
   expect_error(
-    checkVar(emat, c('a', 'a', 'b', 'b')), 
+    checkVar(emat, c('a', 'a', 'b', 'b')),
     paste0('Zero variance in 2 gene-group pairs. ',
            'See _deltaccd_zero_var_genes.csv for full list.'), fixed = TRUE)
 })
 
 test_that('checkGenes', {
   emat = diag(1, 2)
-  rownames(emat) = geneNames 
-  refCor = emat 
+  rownames(emat) = geneNames
+  refCor = emat
   rownames(refCor) = geneNames
-  expect_equal(checkGenes(emat, refCor), geneNames) 
-  
+  expect_equal(checkGenes(emat, refCor), geneNames)
+
   refCor = diag(1, 4)
   rownames(refCor) = paste0('gene_', 1:4)
   expect_error(
-    checkGenes(emat, refCor), 
-    paste0('2 gene(s) is/are not in the expression matrix. ', 
+    checkGenes(emat, refCor),
+    paste0('2 gene(s) is/are not in the expression matrix. ',
            'See _deltaccd_missing_genes.csv for full list.'), fixed = TRUE)
-  
+
   emat = diag(1, 6)
   refCor = diag(1, 4)
   geneNames2 = paste0('gene_', 1:6)
@@ -74,14 +74,14 @@ test_that('checkRefCor', {
   rownames(refCor) = geneNames
   colnames(refCor) = rownames(refCor)
   expect_equal(checkRefCor(refCor), refCor)
-  
+
   expect_error(checkRefCor(), 'Either refCor or refEmat must be supplied.',fixed = TRUE)
-  
+
   rownames(refCor) = paste0('gene_', 3:4)
   expect_error(checkRefCor(refCor),
                'refCor must be a correlation matrix, with identical rownames and colnames.',
                fixed = TRUE)
-  
+
   refCor = rbind(c(1, 0, 0), c(1, 0, 0))
   expect_error(checkRefCor(refCor),
                'refCor must be a correlation matrix, with identical rownames and colnames.',
@@ -89,9 +89,17 @@ test_that('checkRefCor', {
 })
 
 test_that('calcDeltaCCDSimple', {
-  ref = cbind(c(1, -1), c(-1, 1)) 
+  ref = cbind(c(1, -1), c(-1, 1))
   emat = ref
   expect_equal(calcDeltaCCDSimple(ref, emat, 1:2), 0)
+
+  ref = cbind(c(1, 0, -1), c(0, 1, -1), c(-1, -1, 1))
+  emat = cbind(
+    rbind(c(1, 0, 0), c(0, 1, -1), c(0, 1, 1)),
+    rbind(c(-1, 0, -1), c(0, -1, 1), c(-1, 1, 1)))
+  d0 = calcCCDSimple(ref, emat[, 1:3])
+  d1 = calcCCDSimple(ref, emat[, 4:6])
+  expect_equal(calcDeltaCCDSimple(ref, emat, 1:3), d1 - d0)
 })
 
 test_that('makePerms', {
@@ -101,9 +109,9 @@ test_that('makePerms', {
 
 test_that('calcCorr', {
   cors = genCors()
-  
-  expect_equal(cors, data.table(group = c('a', 'b', 'a', 'b'), 
-                                gene1 = factor(paste0('gene_', c(2, 2, 1, 1))), 
+
+  expect_equal(cors, data.table(group = c('a', 'b', 'a', 'b'),
+                                gene1 = factor(paste0('gene_', c(2, 2, 1, 1))),
                                 gene2 = factor(paste0('gene_', c(1, 1, 2, 2)),
                                                levels = paste0('gene_', 2:1)),
                                 rho = c(1, -1, 1, -1)))
